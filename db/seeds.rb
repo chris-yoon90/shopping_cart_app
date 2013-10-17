b5b2811a4a0a4e5336a7305b9567a57042c7ff96
Chris <chris.yoon90@gmail.com>
2013-10-17 00:59:40 -0700

    Created Product scaffold

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.delete_all
+Product.create(title: 'TestProduct1',
+  description: 
+    %{<p>
+        This is a test product number1
+      </p>},
+  image_url:   'cs.jpg',    
+  price: 36.00)
+
+Product.create(title: 'TestProduct2',
+  description:
+    %{<p>
+       This is a test product number 2
+      </p>},
+  image_url: 'ruby.jpg',
+  price: 49.95)
+
+Product.create(title: 'TestProduct3',
+  description: 
+    %{<p>
+        This is a test product number 3.
+      </p>},
+  image_url: 'rtp.jpg',
+  price: 34.95)
