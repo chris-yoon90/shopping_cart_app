3fdf7d4d3a1a796bf7f2d3ea976c26a1b2ccd528
Chris Yoon <chris.yoon90@gmail.com>
2013-10-22 20:55:55 -0700

    added quantity column to line items

--- spec/controllers/line_items_controller_spec.rb
+++ spec/controllers/line_items_controller_spec.rb
@@ -23,7 +23,7 @@ describe LineItemsController do
   # This should return the minimal set of attributes required to create a valid
   # LineItem. As you add validations to LineItem, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) { { "product_id" => "1" } }
+  let(:valid_attributes) { { "product_id" => "1", "cart_id" => "1" } }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
